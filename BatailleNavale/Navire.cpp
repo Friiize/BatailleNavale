c48ef5a693d225fd381d8041797d28ce4e4479f4
Friiize <57799548+Friiize@users.noreply.github.com>
2022-01-17 10:09:36 +0100

    class Navire v1

--- BatailleNavale/Navire.cpp
+++ BatailleNavale/Navire.cpp
@@ -1,1 +1,44 @@
 #include "Navire.h"
+
+Navire::Navire(NavireType type, bool isHoriz, int x, int y)
+{
+	this->type = type;
+	this->isHoriz = isHoriz;
+	this->x = x;
+	this->y = y;
+}
+
+void Navire::SetIsHoriz(bool x)
+{
+	this->isHoriz = x;
+}
+
+void Navire::SetNavireType(NavireType x)
+{
+	this->type = x;
+}
+
+void Navire::SetX(int x)
+{
+	this->x = x;
+}
+
+void Navire::SetY(int y)
+{
+	this->y = y;
+}
+
+bool Navire::GetIsHoriz()
+{
+	return this->isHoriz;
+}
+
+NavireType Navire::GetNavireType()
+{
+	return this->type;
+}
+
+int Navire::GetPos()
+{
+	return this->x, this->y;
+}
--- BatailleNavale/Navire.h
+++ BatailleNavale/Navire.h
@@ -6,7 +6,17 @@ protected:
 	bool isHoriz;
 	NavireType type;
 	int x, y;
+public:
+	Navire();
+	Navire(NavireType type, bool isHoriz, int x, int y);
 
-	
+	bool GetIsHoriz();
+	NavireType GetNavireType();
+	int GetPos(); 
+
+	void SetIsHoriz(bool x);
+	void SetNavireType(NavireType x);
+	void SetX(int x);
+	void SetY(int y);
 };
 
